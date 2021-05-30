b2a3a7bf251363d4adc86b2abfc3c6ad659b566b
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-05-30 16:39:52 +0900

    rubocopを導入

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ end
 
 gem 'devise'
 gem 'pry-rails'
+gem 'rubocop', require: false
+  gem 'rubocop-rails'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"yamada"}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password}
-    password_confirmation {password}
-    family_name           {"山田"}
-    first_name            {"太郎"}
-    family_name_kana      {"ヤマダ"}
-    first_name_kana       {"タロウ"}
-    birth_day             {"1990-01-01"}
+    nickname              { 'yamada' }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password }
+    password_confirmation { password }
+    family_name           { '山田' }
+    first_name            { '太郎' }
+    family_name_kana      { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birth_day             { '1990-01-01' }
   end
 end
