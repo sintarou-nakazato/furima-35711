a2f0520bd91f149c9770d76b31193b33031c9dff
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-05-31 16:05:05 +0900

    出品ボタンを押したら、newにいくようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,13 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :product_description, :category_id, :cost_id, :area_id, :day_id, :price, :image)
+  end
 end
