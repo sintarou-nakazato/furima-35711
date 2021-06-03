db66a15cc265cfcf7621971275b1586499939e1b
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-06-03 11:38:38 +0900

    rubocop

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,13 +5,13 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :price, numericality: { with: /\A[0-9]+\z/ }
     validates :image
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :condition_id
-    validates :cost_id
-    validates :area_id
-    validates :day_id
-  end
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :cost_id
+      validates :area_id
+      validates :day_id
+    end
   end
 
   belongs_to :user
