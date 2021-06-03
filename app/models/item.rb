97ca8e17c386a69f497cfd7b863fb7cc5987021e
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-06-03 16:51:41 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
-  belongs_to_active_hash :Condition
+  belongs_to_active_hash :condition
   belongs_to_active_hash :cost
   belongs_to_active_hash :area
   belongs_to_active_hash :day
