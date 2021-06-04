c9f8c223d21244dd5f3134b5b39f5edee5169d02
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-06-04 12:25:36 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-  before_action :set_questions, only: [:edit, :update]
+  before_action :set_questions, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
