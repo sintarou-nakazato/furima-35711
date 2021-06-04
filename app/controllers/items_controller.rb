b57abe9085ac806b28d9075e3a363c7a8fe8e698
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-06-04 11:57:41 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_question, only: [:show, :edit, :update]
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
   before_action :set_questions, only: [:edit, :update]
 
   def index
@@ -31,6 +31,12 @@ class ItemsController < ApplicationController
       redirect_to items_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to items_path
     end
   end
 
