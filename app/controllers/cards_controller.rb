427cf47a119a36f9391439b4c71b868c517400ac
sintarou-nakazato <83529597+sintarou-nakazato@users.noreply.github.com>
2021-06-08 12:13:07 +0900

    テストコード

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,12 @@
 class CardsController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @card_destination = CardDestination.new
     @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def create
---
+++ spec/factories/card_destinations.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :card_destination do
+    postal_code            {}
+    prefecture_id          {}
+    city                   {}
+    address                {}
+    building_name          {}
+    phone_number           {}
+  end
+end
